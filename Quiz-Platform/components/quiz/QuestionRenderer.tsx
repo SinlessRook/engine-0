74e0159b55186cd895d3e8f9ece341d2e234df05
sinlessrook <asadithyan141@gmail.com>
2026-06-09 01:11:59 +0530

    Fix the short-answer issue

--- Quiz-Platform/components/quiz/QuestionRenderer.tsx
+++ Quiz-Platform/components/quiz/QuestionRenderer.tsx
@@ -96,6 +96,7 @@ function normalizeQuestion(question: Question): Question {
       normalized.question = question.questionText;
     }
   } else if (question.type === 'short_answer' || question.type === 'short-answer') {
+    normalized.type = 'short-answer';
     if (question.questionText && !question.question) {
       normalized.question = question.questionText;
     }
